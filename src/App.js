6dc8a8712e241d86ad1408921ac6f6675d44019d
AltherLago <ajsl@ic.ufal.br>
2022-11-03 15:43:48 -0300

    Home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import Header from './components/header/Header'
+import Home from './components/home/Home'
 
 
 function App() {
@@ -8,6 +9,9 @@ function App() {
 		<div className="App">
 			<>
 				<Header/>
+				<main className='main'>
+					<Home/>
+				</main>
 			</>
         </div>
     );
