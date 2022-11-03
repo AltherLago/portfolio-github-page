42c807e7d8086e1a3f3cf1690a0a6e3c89a65d3f
AltherLago <ajsl@ic.ufal.br>
2022-11-03 17:50:14 -0300

    Home back

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import React from 'react';
 import Header from './components/header/Header'
 import Home from './components/home/Home'
-import About from './components/about/About'
-import Skills from './components/skills/Skills'
-import Portfolio from './components/portfolio/Portfolio';
+// import About from './components/about/About'
+// import Skills from './components/skills/Skills'
+// import Portfolio from './components/portfolio/Portfolio';
 // import Footer from './components/footer/Footer';
 // import Scrollup from './components/scrollup/Scrollup';
 
@@ -15,8 +15,7 @@ function App() {
 				<Header/>
 				<main className='main'>
 					<Home/>
-					<About/> 
-               		<Skills/>
+
 				</main>
 			</>
         </div>
