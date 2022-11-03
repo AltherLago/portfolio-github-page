e5a6ee2539405c403b7a4dadd7dc40d332b02dc7
AltherLago <ajsl@ic.ufal.br>
2022-11-03 17:47:41 -0300

    About && Skills

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import './App.css';
 import React from 'react';
 import Header from './components/header/Header'
 import Home from './components/home/Home'
-
+import About from './components/about/About'
+import Skills from './components/skills/Skills'
+import Portfolio from './components/portfolio/Portfolio';
+// import Footer from './components/footer/Footer';
+// import Scrollup from './components/scrollup/Scrollup';
 
 function App() {
 	return (
@@ -11,6 +15,8 @@ function App() {
 				<Header/>
 				<main className='main'>
 					<Home/>
+					<About/> 
+               		<Skills/>
 				</main>
 			</>
         </div>
